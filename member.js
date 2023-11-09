cd389faca0bac00feaadcefdc31bc9339231cfb9
TreeGround <35868715+Pathwanderer@users.noreply.github.com>
2023-11-09 13:05:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var skills = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  document.getElementById("member").innerHTML = skills.name + " is " + skills.age + " years old.";
+}
